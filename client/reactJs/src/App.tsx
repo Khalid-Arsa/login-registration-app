938b64b6558a500a083ed2cdfa890fa27abf6929
Khalid Sanggoyod <khalid.sanggoyod@fligno.com>
2023-12-28 11:11:58 +0800

    auth context pending

--- client/reactJs/src/App.tsx
+++ client/reactJs/src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const App: React.FC = () => {
   return (
     <>
-      <RouterProvider router={router} />
+      <AuthContextProvider>
+        <RouterProvider router={router} />
+      </AuthContextProvider>
     </>
   );
 };
