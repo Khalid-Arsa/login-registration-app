051c09c74b3ea175034ffcc773eb5ecd174db50c
Khalid Sanggoyod <khalid.sanggoyod@fligno.com>
2023-12-31 13:21:32 +0800

    client: add yup package

--- client/reactJs/src/utils/validation.ts
+++ client/reactJs/src/utils/validation.ts
@@ -1,22 +1,19 @@
-export const authValidation = (obj: any, setState: any) => {
-  const first_name =
-    obj.first_name.length > 0 ? "" : "Please enter your first name";
-  const last_name =
-    obj.last_name.length > 0 ? "" : "Please enter your last name";
-  const address = obj.address.length > 0 ? "" : "Please enter your address";
-  const phone_number =
-    obj.phone_number.length > 0 ? "" : "Please enter your phone number";
-  const email = obj.email.length > 0 ? "" : "Please enter your email";
-  const password = obj.password.length > 0 ? "" : "Please enter your password";
-  const confirm_password =
-    obj.confirm_password.length > 0 ? "" : "Please enter your confirm password";
-  setState({
-    first_name: first_name,
-    last_name: last_name,
-    address: address,
-    phone_number: phone_number,
-    email: email,
-    password: password,
-    confirm_password: confirm_password,
-  });
-};
+import * as Yup from "yup";
+
+export const SignupSchema = Yup.object().shape({
+  first_name: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("First name required"),
+  last_name: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Last name required"),
+  address: Yup.string().required("Address required"),
+  phone_number: Yup.string().required("Phone number required"),
+  email: Yup.string()
+    .email("Email must be a valid email address")
+    .required("Email is required"),
+  password: Yup.string().min(8).required("Password is required"),
+  confirm_password: Yup.string().min(8).required("Confirm password is required"),
+});
